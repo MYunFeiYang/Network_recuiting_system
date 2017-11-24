90b2616032343c57af9b35df44baf36b79d5aa20
MYunFeiYang <2728363142@qq.com>
2017-11-24 12:08:18 +0800

    create custom marquee

--- WebContent/javascript/util/marquee.js
+++ WebContent/javascript/util/marquee.js
@@ -2,15 +2,28 @@
 function marquee() {
         var ul=document.getElementById("marquee").getElementsByTagName("ul")[0];
         var liList=ul.getElementsByTagName("li");
+        var ulOffset=ul.offsetTop;
         for (var i=0;i<liList.length;i++){
             var top=liList[i].style.top;
             top=top.replace(/\s+|px/gi,"");
-            top-=20;
+            top-=1;
             liList[i].style.top=top+"px";
+            var liOffset=liList[i].offsetTop;
+            var marginTop=ulOffset-liOffset;
+            if(marginTop>0){
+                var liText=liList[i].innerHTML;
+                var removeLi=liList[i];
+                removeLi.parentNode.removeChild(removeLi);
+                var li=document.createElement("li");
+                li.innerHTML=liText;
+                var top=li.style.top;
+                top=top.replace(/\s+|px/gi,"");
+                top-=1;
+                li.style.top=top+"px";
+                ul.appendChild(li);
+            }
         };
-        var liText=liList[0].innerHTML;
-        ul.removeChild(liList[0]);
-        var li=document.createElement("li");
-        li.innerHTML=liText;
-        ul.appendChild(li);
     }
+function marquee_control() {
+    setInterval(marquee,1000);
+}
