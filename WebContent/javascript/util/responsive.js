1fedae6317eaa2a7b9284c1da354af6a26ebd145
MYunFeiYang <2728363142@qq.com>
2017-12-12 11:36:20 +0800

    Complete partial response layout and add Left and right change effect for webchat

--- WebContent/javascript/util/responsive.js
+++ WebContent/javascript/util/responsive.js
@@ -1,6 +1,30 @@
-function welt_hidden() {
-    let body_width=document.body.offsetWidth;
-    if (body_width<760){
+"use strict";
+let count=1;
+let mylist ;
+let mydiv ;
 
+function Carousel_figure(count) {
+    let body_width = document.body.offsetWidth;
+    if (body_width <= 768) {
+        mylist = document.getElementById("mylist");
+        mydiv = mylist.children;
+        for (let i = 0; i < mydiv.length; i++) {
+            mydiv[i].classList.add("hidden");
+        }
+        mydiv[count].classList.remove("hidden");
+    }
+}
+
+function mypre() {
+    if (count > 0) {
+        count--;
+        Carousel_figure(count);
+    }
+}
+
+function mynext() {
+    if (mydiv.length-1 > count) {
+        count++;
+        Carousel_figure(count);
     }
 }
