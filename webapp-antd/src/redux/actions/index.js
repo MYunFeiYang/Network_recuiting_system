b1fce70ffcf3d11a8a6ce710e9bfee63240fc95f
thinkway <2728363142@qq.com>
2019-04-26 10:40:38 +0800

    refactor:react技术栈重构前端——添加redux管理状态

--- webapp-antd/src/redux/actions/index.js
+++ webapp-antd/src/redux/actions/index.js
@@ -1,9 +1,32 @@
-import {SET_USER_INFORMATION} from './action_types';
+import {SET_USER_INFORMATION,SET_INDUSTRY_INFORMATION,SET_ADDRESS_INFORMATION,
+  SET_POSITION_INFORMATION,CHANGE_LOGIN_STATE,SET_SELECTED_POSITION,
+SET_SELECTED_ADDRESS,SET_PAGING_INFORMATION} from './action_types';
 
 let actions = {
   setUserInformation: function(payload) {
     return {type: SET_USER_INFORMATION, payload};
-  }
+  },
+  setIndustryInformation: function(payload) {
+    return {type: SET_INDUSTRY_INFORMATION, payload};
+  },
+  setAddressInformation: function(payload) {
+    return {type: SET_ADDRESS_INFORMATION, payload};
+  },
+  setPositionInformation: function(payload) {
+    return {type: SET_POSITION_INFORMATION, payload};
+  },
+  changeLoginState: function(payload) {
+    return {type: CHANGE_LOGIN_STATE, payload};
+  },
+  setSelectedPosition: function(payload) {
+    return {type: SET_SELECTED_POSITION, payload};
+  },
+  setSelectedAddress: function(payload) {
+    return {type: SET_SELECTED_ADDRESS, payload};
+  },
+  setPagingInformation: function(payload) {
+    return {type: SET_PAGING_INFORMATION, payload};
+  },
 };
 
 export default actions;
--- webapp-antd/src/redux/reducers/index.js
+++ webapp-antd/src/redux/reducers/index.js
@@ -1,11 +1,21 @@
-import { SET_USER_INFORMATION } from '../actions/action_types';
+import {
+  SET_USER_INFORMATION, SET_INDUSTRY_INFORMATION, SET_ADDRESS_INFORMATION,
+  SET_POSITION_INFORMATION, CHANGE_LOGIN_STATE, SET_SELECTED_POSITION,
+  SET_SELECTED_ADDRESS, SET_PAGING_INFORMATION
+} from '../actions/action_types';
 let initState = {
   user: {
     nickname: '',
     password: '',
     login_type: ''
   },
-  isLogin:false,
+  isLogin: false,
+  industry: [],
+  address: [],
+  position: [],
+  selectedPosition: '',
+  selectedAddress: '',
+  paging: [{ list: [] }],
 }
 
 function reducer(state = initState, action) {
@@ -13,7 +23,50 @@ function reducer(state = initState, action) {
   switch (action.type) {
     case SET_USER_INFORMATION:
       newState = {
+        ...state,
         user: action.payload
+      };
+      break;
+    case SET_INDUSTRY_INFORMATION:
+      newState = {
+        ...state,
+        industry: action.payload
+      };
+      break;
+    case SET_ADDRESS_INFORMATION:
+      newState = {
+        ...state,
+        address: action.payload
+      };
+      break;
+    case SET_POSITION_INFORMATION:
+      newState = {
+        ...state,
+        position: action.payload
+      };
+      break;
+    case CHANGE_LOGIN_STATE:
+      newState = {
+        ...state,
+        isLogin: action.payload
+      };
+      break;
+    case SET_SELECTED_POSITION:
+      newState = {
+        ...state,
+        selectedPosition: action.payload
+      };
+      break;
+    case SET_SELECTED_ADDRESS:
+      newState = {
+        ...state,
+        selectedAddress: action.payload
+      };
+      break;
+    case SET_PAGING_INFORMATION:
+      newState = {
+        ...state,
+        paging: action.payload
       };
       break;
     default:
